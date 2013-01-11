950f2775ab2fbaecf6c5f220bc70b232a0d66015
Daniel Kaufhold <kaufhold.d@gmail.com>
2013-01-11 16:41:30 +0100

    Added initial migrations for calendarium and test_app.

---
+++ calendarium/tests/test_app/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'DummyModel'
+        db.create_table('test_app_dummymodel', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('content', self.gf('django.db.models.fields.CharField')(max_length=32)),
+        ))
+        db.send_create_signal('test_app', ['DummyModel'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'DummyModel'
+        db.delete_table('test_app_dummymodel')
+
+
+    models = {
+        'test_app.dummymodel': {
+            'Meta': {'object_name': 'DummyModel'},
+            'content': ('django.db.models.fields.CharField', [], {'max_length': '32'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        }
+    }
+
+    complete_apps = ['test_app']
