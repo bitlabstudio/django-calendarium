d3e8bd2b2bb41cb47d1f52d61e04c96913abe8ee
Daniel Kaufhold <kaufhold.d@gmail.com>
2013-01-11 16:20:58 +0100

    Added test_app with DummyModel and DummyModelFactory

---
+++ calendarium/tests/test_app/models.py
@@ -0,0 +1,23 @@
+"""Models for the ``test_app`` test app."""
+import factory
+
+from django.db import models
+
+
+class DummyModel(models.Model):
+    """
+    This is a dummy model for testing purposes.
+
+    :content: Just a dummy field.
+
+    """
+    content = models.CharField(
+        max_length=32,
+    )
+
+
+class DummyModelFactory(factory.Factory):
+    """Factory for the ``DummyModel`` model."""
+    FACTORY_FOR = DummyModel
+
+    content = factory.Sequence(lambda n: 'content{0}'.format(n))
