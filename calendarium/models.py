9538cdbc9a017a4be91f0db898ef127374402765
Daniel Kaufhold <kaufhold.d@gmail.com>
2013-01-12 21:01:50 +0100

    Updated models and tests.
    Added get_occurrences methods and tests for them.
    Additional updates and forgotten field.

--- calendarium/models.py
+++ calendarium/models.py
@@ -1,10 +1,43 @@
-"""Models for the ``calendarium`` app."""
+"""
+Models for the ``calendarium`` app.
+
+The code of these models is highly influenced by or taken from the models of
+django-schedule:
+
+https://github.com/thauber/django-schedule/tree/master/schedule/models
+
+"""
+import json
+from dateutil import rrule
+
 from django.contrib.contenttypes import generic
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from calendarium.constants import FREQUENCY_CHOICES
+from calendarium.utils import OccurrenceReplacer
+
+
+class EventModelManager(models.Manager):
+    """Custom manager for the ``Event`` model class."""
+    def get_occurrences(self, start, end):
+        """Returns a list of events and occurrences for the given period."""
+        # get events of which the end_recurring_period is after the start
+        # of this period and the start is before the end of this period
+        # if they do not have a end_recurring_period, their end needs to end
+        # before the period starts
+
+        # get all occurrences for those events that don't already have a
+        # persistent match and that lie in this period.
+            # This should be done by calling the get_occurrences() method of
+            # each event previously fetched.
+
+            # the get_occurences method should itself replace all persistent
+            # occurrences for the calling event, compute events that are in the
+            # given period and so only return appendable data
+
+        # sort and return
 
 
 class EventModelMixin(models.Model):
@@ -73,11 +106,82 @@ class Event(EventModelMixin):
     rule = models.ForeignKey(
         'Rule',
         verbose_name=_('Rule'),
+        blank=True, null=True,
     )
 
     end_recurring_period = models.DateTimeField(
         verbose_name=_('End of recurring'),
     )
+
+    def _create_occurrence(self, occ_start, occ_end=None):
+        """Creates an Occurrence instance."""
+        # if the length is not altered, it is okay to only pass occ_start
+        if not occ_end:
+            occ_end = occ_start + (self.end - self.start)
+        return Occurrence(
+            event=self, start=occ_start, end=occ_end,
+            # TODO not sure why original start and end also are occ_start/_end
+            original_start=occ_start, original_end=occ_end,
+            title=self.title, description=self.description,
+            creation_date=self.creation_date, created_by=self.created_by)
+
+    def _get_occurrence_list(self, start, end):
+        """Computes all occurrences for this event from start to end."""
+        # get length of the event
+        length = self.end - self.start
+
+        if self.rule:
+            # if the end of the recurring period is before the end arg passed
+            # the end of the recurring period should be the new end
+            if self.end_recurring_period and self.end_recurring_period < end:
+                end = self.end_recurring_period
+            # get all the starts of the occs that end in the poriod
+            rr = self.get_rrule_object()
+            occ_start_list = rr.between(start - length, end, inc=True)
+            # create the occurrences of the period
+            occurrences = []
+            for occ_start in occ_start_list:
+                occ_end = occ_start + length
+                occurrences.append(self._create_occurrence(occ_start, occ_end))
+            return occurrences
+
+    def get_occurrences(self, start, end):
+        """Returns all occurrences from start to end."""
+        # get persistent occurrences
+        # TODO already filter instead of adding them in the end?
+        persistent_occurrences = self.occurrences.all()
+
+        # setup occ_replacer with p_occs
+        occ_replacer = OccurrenceReplacer(persistent_occurrences)
+
+        # compute own occurrences according to rule that overlap with the
+        # period
+        occurrences = self._get_occurrence_list(start, end)
+        # merge computed with persistent using the occ_replacer
+        final_occurrences = []
+        for occ in occurrences:
+            # check if there is a matching persistent occ and get it if true
+            if occ_replacer.has_occurrence(occ):
+                p_occ = occ_replacer.get_occurrence(occ)
+
+                # if the persistent occ falls into the period, replace it
+                if p_occ.start < end and p_occ.end >= start:
+                    final_occurrences.append(p_occ)
+            else:
+                # if there is no persistent match, use the original occ
+                final_occurrences.append(occ)
+        # then add persisted occurrences which originated outside of this
+        # period but now fall within it
+        final_occurrences += occ_replacer.get_additional_occurrences(
+            start, end)
+        return final_occurrences
+
+    def get_rrule_object(self):
+        """Returns the rrule object for this ``Event``."""
+        if self.rule:
+            params = self.rule.get_params()
+            frequency = 'rrule.{0}'.format(self.rule.frequency)
+            return rrule.rrule(eval(frequency), dtstart=self.start, **params)
 
 
 class EventCategory(models.Model):
@@ -165,6 +269,10 @@ class Occurrence(EventModelMixin):
         verbose_name=_('Original end'),
     )
 
+    cancelled = models.BooleanField(
+        verbose_name=_('Cancelled'),
+    )
+
 
 class Rule(models.Model):
     """
@@ -196,3 +304,6 @@ class Rule(models.Model):
         verbose_name=_("params"),
         blank=True, null=True,
     )
+
+    def get_params(self):
+        return json.loads(self.params)
--- calendarium/tests/models_tests.py
+++ calendarium/tests/models_tests.py
@@ -1,23 +1,48 @@
 """Tests for the models of the ``calendarium`` app."""
 from django.test import TestCase
+from django.utils.timezone import timedelta
 
 from calendarium.models import (
-    Event,
     EventCategory,
     EventRelation,
     Occurrence,
     Rule,
 )
+from calendarium.tests.factories import EventFactory, OccurrenceFactory
+from calendarium.utils import now
 
 
 class EventTestCase(TestCase):
     """Tests for the ``Event`` model."""
     longMessage = True
 
-    def test_instantiation(self):
-        """Test for instantiation of the ``Event`` model."""
-        event = Event()
-        self.assertTrue(event)
+    def setUp(self):
+        self.event = EventFactory()
+        self.occurrence = OccurrenceFactory(
+            event=self.event, title='foo_occurrence')
+
+    def test_create_occurrence(self):
+        """Test for ``_create_occurrence`` method."""
+        occurrence = self.event._create_occurrence(now())
+        self.assertEqual(type(occurrence), Occurrence, msg=(
+            'Method ``_create_occurrence`` did not output the right type.'))
+
+    def test_get_occurrence_list(self):
+        """Test for the ``_get_occurrence_list`` method"""
+        occurrence_list = self.event._get_occurrence_list(
+            now(), now() + timedelta(days=8))
+        self.assertEqual(len(occurrence_list), 7, msg=(
+            'The method ``_get_occurrence_list`` did not return the expected'
+            ' amount of items.'))
+
+    def test_get_occurrences(self):
+        occurrences = self.event.get_occurrences(
+            now(), now() + timedelta(days=7))
+        self.assertEqual(len(occurrences), 7, msg=(
+            'Method ``get_occurrences`` did not output the correct amount'
+            ' of occurrences.'))
+        self.assertEqual(occurrences[0].title, 'foo_occurrence', msg=(
+            'The persistent occurrence should have been first in the list.'))
 
 
 class EventCategoryTestCase(TestCase):
