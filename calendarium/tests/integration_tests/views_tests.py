3e9d07a5549bff07794fa3c07c133ea91fe2cc7b
Daniel Kaufhold <kaufhold.d@gmail.com>
2013-01-16 12:05:40 +0100

    Added calendar views.

---
+++ calendarium/tests/integration_tests/views_tests.py
@@ -0,0 +1,88 @@
+"""Tests for the views of the ``calendarium`` app."""
+from django.test import TestCase
+
+from django_libs.tests.mixins import ViewTestMixin
+
+
+class MonthViewTestCase(ViewTestMixin, TestCase):
+    """Tests for the ``MonthView`` view class."""
+    def get_view_name(self):
+        return 'calendar_month'
+
+    def get_view_kwargs(self):
+        return {'year': self.year, 'month': self.month}
+
+    def setUp(self):
+        self.year = 2012
+        self.month = 9
+
+    def test_view(self):
+        """Test for the ``MonthView`` view class."""
+        # regular call
+        resp = self.is_callable()
+        self.assertEqual(
+            resp.template_name[0], 'calendarium/calendar_month.html', msg=(
+                'Returned the wrong template.'))
+
+        # AJAX call
+        resp = self.client.get(
+            self.get_url(), HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(
+            resp.template_name[0], 'calendarium/partials/calendar_month.html',
+            msg=('Returned the wrong template for AJAX request.'))
+
+        # called with wrong values
+        self.is_not_callable(kwargs={'year': 2000, 'month': 15})
+
+
+class WeekViewTestCase(ViewTestMixin, TestCase):
+    """Tests for the ``WeekView`` view class."""
+    def get_view_name(self):
+        return 'calendar_week'
+
+    def get_view_kwargs(self):
+        return {'year': self.year, 'week': self.week}
+
+    def setUp(self):
+        self.year = 2001
+        self.week = 22
+
+    def test_view(self):
+        """Tests for the ``WeekView`` view class."""
+        resp = self.is_callable()
+        self.assertEqual(
+            resp.template_name[0], 'calendarium/calendar_week.html', msg=(
+                'Returned the wrong template.'))
+
+        resp = self.client.get(
+            self.get_url(), HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(
+            resp.template_name[0], 'calendarium/partials/calendar_week.html',
+            msg=('Returned the wrong template for AJAX request.'))
+
+
+class DayViewTestCase(ViewTestMixin, TestCase):
+    """Tests for the ``DayView`` view class."""
+    def get_view_name(self):
+        return 'calendar_day'
+
+    def get_view_kwargs(self):
+        return {'year': self.year, 'month': self.month, 'day': self.day}
+
+    def setUp(self):
+        self.year = 2001
+        self.month = 2
+        self.day = 15
+
+    def test_view(self):
+        """Tests for the ``DayView`` view class."""
+        resp = self.is_callable()
+        self.assertEqual(
+            resp.template_name[0], 'calendarium/calendar_day.html', msg=(
+                'Returned the wrong template.'))
+
+        resp = self.client.get(
+            self.get_url(), HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(
+            resp.template_name[0], 'calendarium/partials/calendar_day.html',
+            msg=('Returned the wrong template for AJAX request.'))
---
+++ calendarium/views.py
@@ -0,0 +1,73 @@
+"""Views for the ``calendarium`` app."""
+import calendar
+
+from django.http import Http404
+from django.utils.timezone import datetime, timedelta
+from django.views.generic import TemplateView
+
+from calendarium.models import Event
+from calendarium.utils import monday_of_week
+
+
+class MonthView(TemplateView):
+    """View to return all occurrences of an event for a whole month."""
+    template_name = 'calendarium/calendar_month.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.month = int(kwargs.get('month'))
+        self.year = int(kwargs.get('year'))
+        if self.month not in range(1, 13):
+            raise Http404
+        if request.is_ajax():
+            self.template_name = 'calendarium/partials/calendar_month.html'
+        return super(MonthView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        month_range = calendar.monthrange(self.year, self.month)
+        first = datetime(year=self.year, month=self.month, day=1)
+        last = datetime(year=self.year, month=self.month, day=month_range[1])
+        occurrences = Event.objects.get_occurrences(first, last)
+        ctx = {'occurrences': occurrences}
+        return ctx
+
+
+class WeekView(TemplateView):
+    """View to return all occurrences of an event for one week."""
+    template_name = 'calendarium/calendar_week.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.week = int(kwargs.get('week'))
+        self.year = int(kwargs.get('year'))
+        if self.week not in range(1, 52):
+            raise Http404
+        if request.is_ajax():
+            self.template_name = 'calendarium/partials/calendar_week.html'
+        return super(WeekView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        monday = monday_of_week(self.year, self.week)
+        sunday = monday + timedelta(days=7)
+        occurrences = Event.objects.get_occurrences(monday, sunday)
+        ctx = {'occurrences': occurrences}
+        return ctx
+
+
+class DayView(TemplateView):
+    """View to return all occurrences of an event for one day."""
+    template_name = 'calendarium/calendar_day.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.day = int(kwargs.get('day'))
+        self.month = int(kwargs.get('month'))
+        self.year = int(kwargs.get('year'))
+        if self.month not in range(1, 13):
+            raise Http404
+        if request.is_ajax():
+            self.template_name = 'calendarium/partials/calendar_day.html'
+        return super(DayView, self).dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        date = datetime(year=self.year, month=self.month, day=self.day)
+        occurrences = Event.objects.get_occurrences(date, date)
+        ctx = {'occurrences': occurrences}
+        return ctx
