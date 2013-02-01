8f8458c8091a0a9b1d5e53ba7c52025419b09368
Daniel Kaufhold <kaufhold.d@gmail.com>
2013-02-01 15:48:15 +0100

    Added OccurrenceForm

---
+++ calendarium/forms.py
@@ -0,0 +1,126 @@
+"""Forms for the ``calendarium`` app."""
+from django import forms
+from django.contrib.auth.models import User
+from django.forms.models import model_to_dict
+from django.utils.timezone import datetime, timedelta
+
+from calendarium.constants import (
+    OCCURRENCE_DECISION_CHOICESS,
+    OCCURRENCE_DECISIONS,
+)
+from calendarium.models import Occurrence
+
+
+class OccurrenceForm(forms.ModelForm):
+    """A form for the ``Occurrence`` model."""
+    decision = forms.CharField(
+        widget=forms.Select(choices=OCCURRENCE_DECISION_CHOICESS),
+    )
+
+    cancelled = forms.BooleanField(
+        widget=forms.HiddenInput,
+        required=False,
+    )
+
+    original_start = forms.DateTimeField(
+        widget=forms.HiddenInput,
+    )
+
+    original_end = forms.DateTimeField(
+        widget=forms.HiddenInput,
+    )
+
+    class Meta:
+        model = Occurrence
+
+    def clean(self):
+        cleaned_data = super(OccurrenceForm, self).clean()
+        return cleaned_data
+
+    def save(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data['decision'] == OCCURRENCE_DECISIONS['all']:
+            changes = {
+                key: value for key, value in cleaned_data.iteritems()
+                if value != self.initial.get(key) and self.initial.get(key)}
+            event = self.instance.event
+            # for each field on the event, check for new data in cleaned_data
+            for field_name in [field.name for field in event._meta.fields]:
+                value = changes.get(field_name)
+                if value:
+                    setattr(event, field_name, value)
+            event.save()
+
+            # repeat for persistent occurrences
+            for occ in event.occurrences.all():
+                for field_name in [field.name for field in occ._meta.fields]:
+                    value = changes.get(field_name)
+                    if value:
+                        # since we can't just set a new datetime, we have to
+                        # adjust the datetime fields according to the changes
+                        # on the occurrence form instance
+                        if type(value) != datetime:
+                            setattr(occ, field_name, value)
+                        else:
+                            initial_time = self.initial.get(field_name)
+                            occ_time = getattr(occ, field_name)
+                            delta = value - initial_time
+                            new_time = occ_time + delta
+                            setattr(occ, field_name, new_time)
+                occ.save()
+
+            # get everything from initial and compare to cleaned_data to
+            # retrieve what has been changed
+            # apply those changes to the persistent occurrences (and the main
+            # event)
+        elif cleaned_data['decision'] == OCCURRENCE_DECISIONS['this one']:
+            self.instance.save()
+        elif cleaned_data['decision'] == OCCURRENCE_DECISIONS['following']:
+            # get the changes
+            changes = {
+                key: value for key, value in cleaned_data.iteritems()
+                if value != self.initial.get(key) and self.initial.get(key)}
+
+            # change the old event
+            old_event = self.instance.event
+            end_recurring_period = self.instance.event.end_recurring_period
+            old_event.end_recurring_period = self.instance.start - timedelta(
+                days=1)
+            old_event.save()
+
+            # the instance occurrence holds the info for the new event, that we
+            # use to update the old event's fields
+            new_event = old_event
+            new_event.end_recurring_period = end_recurring_period
+            new_event.id = None
+            event_kwargs = model_to_dict(self.instance)
+            for field_name in [field.name for field in new_event._meta.fields]:
+                if field_name == 'created_by':
+                    value = User.objects.get(pk=event_kwargs.get(field_name))
+                elif field_name in ['rule', 'category']:
+                    continue
+                else:
+                    value = event_kwargs.get(field_name)
+                if value:
+                    setattr(new_event, field_name, value)
+            new_event.save()
+
+            # update the new event and update all of the corresponding occs
+            for occ in self.instance.event.occurrences.filter(
+                    start__gte=self.instance.start):
+                occ.event = new_event
+                for field_name in [field.name for field in occ._meta.fields]:
+                    value = changes.get(field_name)
+                    if value:
+                        # since we can't just set a new datetime, we have to
+                        # adjust the datetime fields according to the changes
+                        # on the occurrence form instance
+                        if type(value) != datetime:
+                            setattr(occ, field_name, value)
+                        else:
+                            initial_time = self.initial.get(field_name)
+                            occ_time = getattr(occ, field_name)
+                            delta = value - initial_time
+                            new_time = occ_time + delta
+                            setattr(occ, field_name, new_time)
+                occ.save()
---
+++ calendarium/tests/forms_tests.py
@@ -0,0 +1,130 @@
+"""Tests for the forms of the ``calendarium`` app."""
+import json
+
+from django.forms.models import model_to_dict
+from django.test import TestCase
+from django.utils.timezone import timedelta
+
+from calendarium.constants import FREQUENCIES, OCCURRENCE_DECISIONS
+from calendarium.forms import OccurrenceForm
+from calendarium.models import Event, Occurrence
+from calendarium.tests.factories import EventFactory, RuleFactory
+from calendarium.utils import now
+
+
+class OccurrenceFormTestCase(TestCase):
+    """Test for the ``OccurrenceForm`` form class."""
+    longMessage = True
+
+    def setUp(self):
+        # single, not recurring event
+        self.event = EventFactory(rule=None, end_recurring_period=None)
+        self.event_occurrence = self.event.get_occurrences(
+            self.event.start).next()
+
+        # recurring event weekly on mondays over 6 weeks
+        self.rule = RuleFactory(
+            name='weekly', frequency=FREQUENCIES['WEEKLY'],
+            params=json.dumps({'byweekday': 0}))
+        self.rec_event = EventFactory(
+            rule=self.rule, start=now(),
+            set__end_recurring_period=41)
+        self.rec_occurrence_list = [
+            occ for occ in self.rec_event.get_occurrences(
+                self.rec_event.start, self.rec_event.end_recurring_period)]
+        self.rec_occurrence = self.rec_occurrence_list[2]
+
+    def test_form(self):
+        """Test if ``OccurrenceForm`` is valid and saves correctly."""
+        # Test for not recurring event
+        data = model_to_dict(self.event_occurrence)
+        initial = data.copy()
+        data.update({
+            'decision': OCCURRENCE_DECISIONS['all'],
+            'title': 'changed'})
+        form = OccurrenceForm(data=data, initial=initial)
+        self.assertTrue(form.is_valid(), msg=(
+            'The OccurrenceForm should be valid'))
+        form.save()
+        event = Event.objects.get(pk=self.event.pk)
+        self.assertEqual(event.title, 'changed', msg=(
+            'When save is called, the event\'s title should be "changed".'))
+
+        # Test for recurring event
+
+        # Case 1: Altering occurrence 3 to be on a tuesday.
+        data = model_to_dict(self.rec_occurrence)
+        initial = data.copy()
+        data.update({
+            'decision': OCCURRENCE_DECISIONS['this one'],
+            'title': 'different'})
+        form = OccurrenceForm(data=data, initial=initial)
+        self.assertTrue(form.is_valid(), msg=(
+            'The OccurrenceForm should be valid'))
+        form.save()
+        self.assertEqual(Occurrence.objects.all().count(), 1, msg=(
+            'After one occurrence has changed, there should be one persistent'
+            ' occurrence.'))
+        occ = Occurrence.objects.get()
+        self.assertEqual(occ.title, 'different', msg=(
+            'When save is called, the occurrence\'s title should be'
+            ' "different".'))
+
+        # Case 2: Altering the description of "all" on the first occurrence
+        # should also change 3rd one
+        occ_to_use = self.rec_occurrence_list[0]
+        data = model_to_dict(occ_to_use)
+        initial = data.copy()
+        new_start = occ_to_use.start + timedelta(hours=1)
+        data.update({
+            'decision': OCCURRENCE_DECISIONS['all'],
+            'description': 'has changed',
+            'start': new_start})
+        form = OccurrenceForm(data=data, initial=initial)
+        self.assertTrue(form.is_valid(), msg=(
+            'The OccurrenceForm should be valid'))
+        form.save()
+        self.assertEqual(Occurrence.objects.all().count(), 1, msg=(
+            'After one occurrence has changed, there should be one persistent'
+            ' occurrence.'))
+        occ = Occurrence.objects.get()
+        self.assertEqual(occ.title, 'different', msg=(
+            'When save is called, the occurrence\'s title should still be'
+            ' "different".'))
+        self.assertEqual(occ.description, 'has changed', msg=(
+            'When save is called, the occurrence\'s description should be'
+            ' "has changed".'))
+        self.assertEqual(
+            occ.start, self.rec_occurrence.start + timedelta(hours=1), msg=(
+                'When save is called, the occurrence\'s start time should be'
+                ' set forward one hour.'))
+
+        # Case 3: Altering everthing from occurrence 4 to 6 to one day later
+        occ_to_use = self.rec_occurrence_list[4]
+        data = model_to_dict(occ_to_use)
+        initial = data.copy()
+        new_start = occ_to_use.start + timedelta(days=1)
+        data.update({
+            'decision': OCCURRENCE_DECISIONS['following'],
+            'start': new_start})
+        form = OccurrenceForm(data=data, initial=initial)
+        self.assertTrue(form.is_valid(), msg=(
+            'The OccurrenceForm should be valid'))
+        form.save()
+        self.assertEqual(Event.objects.all().count(), 3, msg=(
+            'After changing occurrence 4-6, a new event should have been'
+            ' created.'))
+        event1 = Event.objects.get(pk=self.rec_event.pk)
+        event2 = Event.objects.exclude(
+            pk__in=[self.rec_event.pk, self.event.pk]).get()
+        self.assertEqual(
+            event1.end_recurring_period,
+            event2.start - timedelta(days=1), msg=(
+                'The end recurring period of the old event should be the same'
+                ' as the start of the new event minus one day.'))
+        self.assertEqual(
+            event2.end_recurring_period, self.rec_event.end_recurring_period,
+            msg=(
+                'The end recurring period of the new event should be the'
+                ' old end recurring period of the old event.'))
+        # -> should yield 2 events, one newly created one altered
