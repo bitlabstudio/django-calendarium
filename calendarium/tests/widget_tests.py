b9505960c2f6911173b79036708dfd9f746c115c
Tobias Lorenz <tobias.lorenz@bitmazk.com>
2013-05-05 19:58:06 +0200

    created widget tests

---
+++ calendarium/tests/widget_tests.py
@@ -0,0 +1,17 @@
+"""Tests for the widgets of the ``calendarium`` app."""
+from django.template import Context, Template
+from django.test import TestCase
+
+from ..widgets import ColorPickerWidget
+
+
+class ColorPickerWidgetTestCase(TestCase):
+    """Tests for the ``ColorPickerWidget`` widget."""
+    longMessage = True
+
+    def setUp(self):
+        self.widget = ColorPickerWidget()
+
+    def test_render_tag(self):
+        self.assertIn('value="ffffff"', self.widget.render('field', 'ffffff'),
+                      msg=('Should render the input form.'))
